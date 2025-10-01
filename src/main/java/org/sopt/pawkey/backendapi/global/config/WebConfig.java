a6c36662eb930faf8bfd64c3143e97d96c4f0c3a
dltnals317 <sumin4101@naver.com>
2025-10-01 20:41:38 +0900

    [feat] JwtAuthenticationFilter 추가

--- src/main/java/org/sopt/pawkey/backendapi/global/config/WebConfig.java
+++ src/main/java/org/sopt/pawkey/backendapi/global/config/WebConfig.java
@@ -1,24 +1,24 @@
-package org.sopt.pawkey.backendapi.global.config;
-
-import org.sopt.pawkey.backendapi.global.config.interceptor.UserHeaderInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-	private final UserHeaderInterceptor userHeaderInterceptor;
-
-	public WebConfig(UserHeaderInterceptor userHeaderInterceptor) {
-		this.userHeaderInterceptor = userHeaderInterceptor;
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(userHeaderInterceptor)
-			.addPathPatterns("/api/**")
-			.excludePathPatterns("/api/v1/regions")
-		; // 적용하고 싶은 URL 경로
-	}
-}
+// package org.sopt.pawkey.backendapi.global.config;
+//
+// import org.sopt.pawkey.backendapi.global.config.interceptor.UserHeaderInterceptor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
+//
+// 	private final UserHeaderInterceptor userHeaderInterceptor;
+//
+// 	public WebConfig(UserHeaderInterceptor userHeaderInterceptor) {
+// 		this.userHeaderInterceptor = userHeaderInterceptor;
+// 	}
+//
+// 	@Override
+// 	public void addInterceptors(InterceptorRegistry registry) {
+// 		registry.addInterceptor(userHeaderInterceptor)
+// 			.addPathPatterns("/api/**")
+// 			.excludePathPatterns("/api/v1/regions")
+// 		; // 적용하고 싶은 URL 경로
+// 	}
+// }
