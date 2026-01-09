48813c9bbb313fd39387af26d571e4a104caa603
daeunhan <daeunhan0723@gmail.com>
2026-01-09 22:57:29 +0900

    [feat] facade 구현

--- src/main/java/org/sopt/pawkey/backendapi/domain/dbti/api/dto/response/DbtiResultResponseDto.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/dbti/api/dto/response/DbtiResultResponseDto.java
@@ -1,7 +1,9 @@
 package org.sopt.pawkey.backendapi.domain.dbti.api.dto.response;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.sopt.pawkey.backendapi.domain.dbti.infra.persistence.entity.DbtiEntity;
 import org.sopt.pawkey.backendapi.domain.dbti.infra.persistence.entity.DbtiResultEntity;
 
 public record DbtiResultResponseDto(
@@ -14,14 +16,13 @@ public record DbtiResultResponseDto(
 	int sociality,
 	int routine
 ) {
-	public static DbtiResultResponseDto of(DbtiResultEntity result, String name, String image, List<String> keywords,
-		String description) {
+	public static DbtiResultResponseDto of(DbtiResultEntity result, DbtiEntity dbtiInfo) {
 		return new DbtiResultResponseDto(
 			result.getFinalType(),
-			name,
-			image,
-			keywords,
-			description,
+			dbtiInfo.getName(),
+			dbtiInfo.getImageUrl(),
+			Arrays.asList(dbtiInfo.getKeywords().split(",")),
+			dbtiInfo.getDescription(),
 			result.getEiScore(),
 			result.getPsScore(),
 			result.getRtScore()
---
+++ src/main/java/org/sopt/pawkey/backendapi/domain/dbti/infra/persistence/entity/DbtiResultEntity.java
@@ -0,0 +1,41 @@
+package org.sopt.pawkey.backendapi.domain.dbti.infra.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "dbti_result")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DbtiResultEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "dbti_result_id")
+	private Long id;
+
+	@Column(nullable = false)
+	private Long petId;
+
+	@Column(nullable = false, length = 3)
+	private String dbtiType;
+
+	private int eiScore;
+	private int psScore;
+	private int rtScore;
+	@Builder
+	public DbtiResultEntity(Long petId, String dbtiType, int eiScore, int psScore, int rtScore) {
+		this.petId = petId;
+		this.dbtiType = dbtiType;
+		this.eiScore = eiScore;
+		this.psScore = psScore;
+		this.rtScore = rtScore;
+	}
+}
