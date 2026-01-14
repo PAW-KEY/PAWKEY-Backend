303e6e567079d79d07f3bc2d8f2e9c9e1055caf0
daeunhan <daeunhan0723@gmail.com>
2026-01-15 00:50:43 +0900

    [chore] import문 최적화

--- src/main/java/org/sopt/pawkey/backendapi/domain/dbti/infra/persistence/repository/SpringDataDbtiOptionRepository.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/dbti/infra/persistence/repository/SpringDataDbtiOptionRepository.java
@@ -1,7 +1,5 @@
 package org.sopt.pawkey.backendapi.domain.dbti.infra.persistence.repository;
 
-import java.util.List;
-
 import org.sopt.pawkey.backendapi.domain.dbti.infra.persistence.entity.DbtiOptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
