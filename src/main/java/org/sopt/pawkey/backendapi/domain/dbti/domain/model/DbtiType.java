dffdd025c9d8a681ad850004d0b8c8e6678a68f6
daeunhan <daeunhan0723@gmail.com>
2026-01-15 00:04:46 +0900

    [chore] rt 오타 수정

--- src/main/java/org/sopt/pawkey/backendapi/domain/dbti/domain/model/DbtiType.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/dbti/domain/model/DbtiType.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 public enum DbtiType {
 	EPR, EPF, ESR, ESF, IPR, IPF, ISR, ISF;
 
-	public static String determine(int ei, int ps, int rt) {
+	public static String determine(int ei, int ps, int rf) {
 		return (ei >= 2 ? "E" : "I") +
 			(ps >= 2 ? "P" : "S") +
-			(rt >= 2 ? "R" : "F");
+			(rf >= 2 ? "R" : "F");
 	}
 }
