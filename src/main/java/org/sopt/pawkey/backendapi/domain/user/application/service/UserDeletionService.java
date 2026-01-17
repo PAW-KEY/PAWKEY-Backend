e77b3d0e3e68957818879d8404e98d23cdacf26b
dltnals317 <sumin4101@naver.com>
2026-01-17 16:10:29 +0900

    [feat] 유저 탈퇴 후 재가입시 신규유저 처리 로직 추가 / apple탈퇴 refreshtoken 삭제

--- src/main/java/org/sopt/pawkey/backendapi/domain/user/application/service/UserDeletionService.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/user/application/service/UserDeletionService.java
@@ -1,5 +1,6 @@
 package org.sopt.pawkey.backendapi.domain.user.application.service;
 
+import org.sopt.pawkey.backendapi.domain.auth.domain.repository.AppleRefreshTokenRepository;
 import org.sopt.pawkey.backendapi.domain.user.domain.repository.SocialAccountRepository;
 import org.sopt.pawkey.backendapi.domain.user.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class UserDeletionService {
 	private final UserRepository userRepository;
 	private final SocialAccountRepository socialAccountRepository;
+	private final AppleRefreshTokenRepository appleRefreshTokenRepository;
 
 	@Transactional
 	public void deleteUser(Long userId) {
-		//socialAccountRepository.deleteByUserId(userId);
-		//userRepository.deleteById(userId);
+		appleRefreshTokenRepository.deleteById(userId);
+		socialAccountRepository.deleteByUser_UserId(userId);
+		userRepository.deleteById(userId);
 	}
 }
