1910f11209a65482322ebf0b2851f7061950fc74
daeunhan <daeunhan0723@gmail.com>
2026-01-30 23:26:47 +0900

    [fix] distance를 km단위로 수정

--- src/main/java/org/sopt/pawkey/backendapi/domain/homeWeather/api/dto/HomeInfoResponseDto.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/homeWeather/api/dto/HomeInfoResponseDto.java
@@ -2,14 +2,17 @@ package org.sopt.pawkey.backendapi.domain.homeWeather.api.dto;
 
 public record HomeInfoResponseDto(
 	Double distance,
-	Integer time,
+	Integer totalTime,
 	Integer count
 ) {
-	public static HomeInfoResponseDto of(Double distance, Integer time, Long count) {
-		return new HomeInfoResponseDto(
-			distance != null ? Math.round(distance * 10) / 10.0 : 0.0,
-			time != null ? time : 0,
-			count != null ? count.intValue() : 0
-		);
+	public HomeInfoResponseDto {
+		distance = distance != null ? Math.round((distance / 1000.0) * 10) / 10.0 : 0.0;
+
+		totalTime = totalTime != null ? totalTime : 0;
+		count = count != null ? count : 0;
+	}
+
+	public static HomeInfoResponseDto of(Double distance, Integer totalTime, Integer count) {
+		return new HomeInfoResponseDto(distance, totalTime, count);
 	}
 }
