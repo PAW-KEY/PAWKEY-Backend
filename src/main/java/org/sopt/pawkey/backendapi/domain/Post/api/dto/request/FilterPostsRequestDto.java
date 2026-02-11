1bb3487b49a62f6171950f2b692646416fa3a158
daeunhan <daeunhan0723@gmail.com>
2026-02-11 20:10:14 +0900

    [chore] 스웨거 문구 추가

--- src/main/java/org/sopt/pawkey/backendapi/domain/Post/api/dto/request/FilterPostsRequestDto.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/Post/api/dto/request/FilterPostsRequestDto.java
@@ -2,18 +2,30 @@ package org.sopt.pawkey.backendapi.domain.post.api.dto.request;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record FilterPostsRequestDto(
 	List<CategoryFilterDto> selectedOptions
 ) {
 	public record CategoryFilterDto(
 		Long categoryId,
 		Long durationId,
+
+		@Schema(
+			description = "옵션 ID 세트 (단일 숫자 22 혹은 배열 [7, 9] 모두 가능)",
+			type = "integer",
+			example = "0"
+		)
 		Object optionsIds // long값과 배열값 모두 처리 가능
 	) {
+		@JsonIgnore
 		public Long getTargetId() {
 			return categoryId != null ? categoryId : durationId;
 		}
 
+		@JsonIgnore
 		public List<Long> getOptionIdList() {
 			if (optionsIds instanceof List) { // 리스트(배열) 형태일 떄
 				return ((List<?>)optionsIds).stream()
