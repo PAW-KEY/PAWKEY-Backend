610eebca89f9f08b857a2e0b863490ba4188bed3
daeunhan <daeunhan0723@gmail.com>
2026-02-11 21:23:27 +0900

    [fix] 필터 옵션 관련, 에러 코드 추가

--- src/main/java/org/sopt/pawkey/backendapi/domain/Post/api/dto/request/FilterPostsRequestDto.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/Post/api/dto/request/FilterPostsRequestDto.java
@@ -2,6 +2,9 @@ package org.sopt.pawkey.backendapi.domain.post.api.dto.request;
 
 import java.util.List;
 
+import org.sopt.pawkey.backendapi.domain.post.exception.PostBusinessException;
+import org.sopt.pawkey.backendapi.domain.post.exception.PostErrorCode;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -27,13 +30,18 @@ public record FilterPostsRequestDto(
 
 		@JsonIgnore
 		public List<Long> getOptionIdList() {
-			if (optionsIds instanceof List) { // 리스트(배열) 형태일 떄
-				return ((List<?>)optionsIds).stream()
-					.map(o -> Long.valueOf(o.toString())).toList();
-			} else if (optionsIds != null) { // 단일 값일 때
-				return List.of(Long.valueOf(optionsIds.toString()));
+			try {
+				if (optionsIds instanceof List) { // 리스트(배열) 형태일 때
+					return ((List<?>)optionsIds).stream()
+						.map(o -> Long.valueOf(o.toString()))
+						.toList();
+				} else if (optionsIds != null) { // 단일 값일 때
+					return List.of(Long.valueOf(optionsIds.toString()));
+				}
+			} catch (NumberFormatException e) {
+				throw new PostBusinessException(PostErrorCode.INVALID_FILTER_OPTION);
 			}
-			return List.of(); // 안 들어왔을 때
+			return List.of(); // 값이 들어오지 않았을 때
 		}
 	}
 }
