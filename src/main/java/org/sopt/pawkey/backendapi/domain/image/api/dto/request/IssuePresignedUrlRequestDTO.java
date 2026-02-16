9bcb2a85afa9fe0aa31a26df2f4784b520b4c930
이수민 <sumin4101@naver.com>
2026-02-16 22:27:51 +0900

    [hotfix] swagger에 domainEnum필드 숨김

--- src/main/java/org/sopt/pawkey/backendapi/domain/image/api/dto/request/IssuePresignedUrlRequestDTO.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/image/api/dto/request/IssuePresignedUrlRequestDTO.java
@@ -1,5 +1,7 @@
 package org.sopt.pawkey.backendapi.domain.image.api.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import org.sopt.pawkey.backendapi.domain.image.domain.ImageDomain;
 
@@ -7,6 +9,9 @@ public record IssuePresignedUrlRequestDTO(
 	@NotBlank String domain,      // route, profile , walk...
 	@NotBlank String contentType
 ) {
+
+	@JsonIgnore
+	@Schema(hidden = true)
 	public ImageDomain getDomainEnum(){
 		return ImageDomain.from(this.domain);
 
