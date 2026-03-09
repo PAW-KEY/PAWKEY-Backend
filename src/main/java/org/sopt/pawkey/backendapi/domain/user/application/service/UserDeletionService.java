1470faf134d026eb8821e4cbce5f26e20fffb1ec
이수민 <sumin4101@naver.com>
2026-03-09 22:59:44 +0900

    [fix] 연관관계 오류 해결

--- src/main/java/org/sopt/pawkey/backendapi/domain/user/application/service/UserDeletionService.java
+++ src/main/java/org/sopt/pawkey/backendapi/domain/user/application/service/UserDeletionService.java
@@ -40,9 +40,7 @@ public class UserDeletionService {
 
 
 		//연관 테이블 삭제
-		//postRepository.deleteByRouteUserId(userId);
-		List<PostEntity> posts = postRepository.findByRoute_User_UserId(userId);
-		postRepository.deleteAll(posts);
+		postRepository.deleteByUserId(userId);
 
 		entityManager.flush();
 
